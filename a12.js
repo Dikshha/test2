0896da40290675c62bde194644494476c2e1d261
Harshith Bolar <harshith.bolar@gmail.com>
2017-09-18 13:02:42 +0530

    Update a12.js
    Fixed formatting.

--- a12.js
+++ a12.js
@@ -1,3 +1,8 @@
-var e1={id:1001,ename:"Kiran",job:"architecture",salary:125000};
-console.log(e1.id+" "+e1.ename+" "+e1.job+" "+e1.salary);
-this is example
+var e1 = {
+  id: 1001,
+  ename: "Kiran",
+  job: "architecture",
+  salary: 125000
+};
+
+console.log(e1.id + " " + e1.ename + " " + e1.job + " " + e1.salary);
