2edf360f5704ba3417f9a2af312582d1b347b84c
Harshith Bolar <harshith.bolar@gmail.com>
2017-09-18 12:56:15 +0530

    Update a12.js
    Removed unwanted lines

--- a12.js
+++ a12.js
@@ -1,9 +1,3 @@
-//var e1={};
-//var e2= new Object();
-
-//console.log(e1);
-//console.log(e2);
-
 var e1={id:1001,ename:"Kiran",job:"architecture",salary:125000};
 console.log(e1.id+" "+e1.ename+" "+e1.job+" "+e1.salary);
 this is example
